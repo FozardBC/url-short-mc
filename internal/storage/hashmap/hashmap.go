69a3d478604a249cc49e50e1645b0639bf936c2a
fozardUb <fzrd2000@gmail.com>
2025-04-16 14:31:28 +0300

    resolved conflicts

--- internal/storage/hashmap/hashmap.go
+++ internal/storage/hashmap/hashmap.go
@@ -12,11 +12,9 @@ type HashmapStorage struct {
 func New() *HashmapStorage {
 	return &HashmapStorage{
 		data: map[string]string{"vk": "https://vk.com"},
-		data: map[string]string{"vk": "https://vk.com"},
 	}
 }
 
-func (m *HashmapStorage) GetURL(alias string) (string, error) {
 func (m *HashmapStorage) GetURL(alias string) (string, error) {
 
 	url, ok := m.data[alias]
@@ -40,15 +38,17 @@ func (m *HashmapStorage) SaveURL(url string, alias string) error {
 }
 
 func (m *HashmapStorage) DeleteURL(alias string) error {
-func (m *HashmapStorage) DeleteURL(alias string) error {
+
 	_, ok := m.data[alias]
 	if !ok {
-	if !ok {
-		return storage.ErrAliasNotFound
+		if !ok {
+			return storage.ErrAliasNotFound
+		}
+
+		delete(m.data, alias)
+
+		return nil
 	}
-
-	delete(m.data, alias)
-
 	return nil
 }
 
